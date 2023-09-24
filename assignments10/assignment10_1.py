e9f2950a7bd95d2974074591e6918eb39c123326
Devesh <deveshachaudhari@gmail.com>
2023-09-24 18:12:32 +0530

    10_1

---
+++ assignments10/assignment10_1.py
@@ -0,0 +1,50 @@
+# Display file with extension
+from sys import *
+import os
+import time
+def DirectoryTravel(DirName):
+    print("We are going to Scan the Directory : ",DirName)
+
+    flag = os.path.isabs(DirName)
+
+    if flag == False:
+        DirName = os.path.abspath(DirName)
+
+    exist = os.path.isdir(DirName)
+
+    if exist:
+        for foldername, subfoldername, filename in os.walk(DirName):
+            print("Current Directory name : ",foldername)
+            for fname in filename:
+                fileExtension = os.path.splitext(fname)
+                if fileExtension[1] == argv[2]:
+                    print(fname)
+    else:
+        print("Invalid path")
+def main():
+    print("-------------- Automation Script --------------")
+
+    print("Automation Script Name : ",argv[0])
+
+    if(len(argv) != 3):
+        print("Invalid number of arguments")
+        exit()
+
+    if(argv[1] == "-h" or argv[1] == "-H"):    # Flag for displaying help
+        print("This automation script is used to display file with given extension")
+        exit()
+    
+    elif(argv[1] == "-u" or argv[1] == "-U"):    # Flag for displaying the usage of script
+        print("Usage : Name_Of_Script First_Argument second_Argument")
+        print("Example : Demo.py Demo .txt")
+        exit()
+
+    else:
+        starttime = time.time()
+        DirectoryTravel(argv[1])
+        endtime = time.time()
+
+        print("The script took time to execute as : ",endtime-starttime)
+
+if __name__ == "__main__":
+    main()
